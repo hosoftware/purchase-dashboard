cc762a834413eec5358d23117ef9fedcaf66b236
Monisha <monisha@ariesmar.com>
2025-09-24 15:22:16 +0400

    new corrections

--- test.py
+++ test.py
@@ -5223,9 +5223,4 @@ def get_purchase_request_statuswise_count():
 
 
 if __name__ == '__main__':
-    application.run(debug=True)
-
-
-if __name__ == '__main__':
-    application.run(debug=True)
-
+    application.run(host="172.16.134.36", port=5000,debug=True)
