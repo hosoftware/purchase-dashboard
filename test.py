7b210b214653865be2bd84e3a6bfca83b81a0c3f
shaheerghub <shaheersha.mail@gmail.com>
2025-10-28 10:35:21 +0400

    redis commented

--- test.py
+++ test.py
@@ -147,7 +147,7 @@ def get_role_code():
         
 @application.route('/api/analytics', methods=['GET'])
 def get_analytics():
-    r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=False)
+    # r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=False)
     cursor = mysql.connection.cursor()
     encoded_user_id = request.args.get('user_id')
     decoded_bytes = base64.b64decode(encoded_user_id)
@@ -158,13 +158,12 @@ def get_analytics():
     employee_id = user_role_query[0][1]
     role_code = user_role_query[0][2]
     cache_key = f"purchase_request_data_{employee_id}"
-    cached_data = r.get(cache_key)
-    return jsonify({"Success": user_role})
+    # cached_data = r.get(cache_key)
 
-    if cached_data:
+    # if cached_data:
         # If data is found in cache, return the cached data
         # print("Using cached data")
-        return pickle.loads(cached_data)
+        # return pickle.loads(cached_data)
     # return({ "user": role_code })
     if user_role == 2:
         cursor.callproc('GenerateYearlyQuery', (userid,))
